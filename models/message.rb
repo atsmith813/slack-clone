734bfefabc3c3c255e219c5425ad12d5d54256a4
Alex <alexandersmith223@gmail.com>
2018-06-08 17:20:41 -0400

    Add additional validation to message model

--- models/message.rb
+++ models/message.rb
@@ -1,5 +1,9 @@
 class Message < ActiveRecord::Base
   belongs_to :channel
   belongs_to :user
+
+  validates :content, presence: true
+  validates :user_id, presence: true
+  validates :channel_id, presence: true
 end
 
